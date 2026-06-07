26e63f7da4b2676ccaa764f5a325bdc7f79ccbfe
Borys Bondarenko <borisbondarenko@gmail.com>
2026-06-07 09:25:40 +0200

    feat: remove sitemap integration from configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
-import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://borysonline.com",
-  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: "github-dark",
